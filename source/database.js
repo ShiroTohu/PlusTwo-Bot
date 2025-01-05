ee1765b30dd960e994bdd0cf267e0db086f96477
shirotohu <shirotohu@shirotohu.dev>
2025-01-05 16:07:56 +1000

    database table creation

--- source/database.js
+++ source/database.js
@@ -1,9 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('node:fs');
 const path = require('node:path');
+const { run } = require('node:test');
 
 function createDatabase() {
     const folder = 'database/'
+    const databaseName = 'database'
+
     if (!fs.existsSync(folder)) {
         fs.mkdirSync(folder, (err) => {
             if (err) {
@@ -13,7 +16,20 @@ function createDatabase() {
         });
     }
 
-    return db = new sqlite3.Database('database/database.db')
+    if (!fs.existsSync(`${folder}/${databaseName}.db`)) {
+        db = new sqlite3.Database(`${folder}/${databaseName}.db`);
+        db.exec(`
+            create table scores (
+                user_id int primary key not null unique,
+                username text,
+                score int
+            );`
+        );
+        return db;
+    } else {
+        db = new sqlite3.Database(`${folder}/${databaseName}.db`);
+        return db;
+    }
 }
 
 module.exports = {
