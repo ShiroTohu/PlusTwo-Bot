463623c8a60f53f73a902ccc9fb23bb17c28ec89
shirotohu <shirotohu@shirotohu.dev>
2025-01-06 12:04:07 +1000

    created event handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 const { Database } = require('./source/database')
+const { Sequelize } = require('sequelize')
 
 // Create a new client instance
 const client = new Client({ 
@@ -17,10 +18,10 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-client.db = Database.createDatabase();
-console.log('database initalized')
+// client.db = Database.createDatabase();
+// console.log('database initalized')
 
-// find the commands folder
+// command handler
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -39,51 +40,25 @@ for (const folder of commandFolders) {
 	}
 }
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+// event handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    client.user.setActivity('https://youtu.be/q2FYfDpEpGc', { type: ActivityType.Watching });
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-    }
-})
-
-client.on(Events.MessageCreate, async message => {
-    if (message.type == 19) {
-        repliedTo = await message.fetchReference();
-        if (message.content === "+2" | message.content === "-2") {
-            message.react('1325312547662073906');
-        }
-    }
-})
-
-process.on('SIGINT', function() {
-    client.db.close()
-    console.log('database closed')
-    process.exit();
-});
+// process.on('SIGINT', function() {
+//     client.db.close()
+//     console.log('database closed')
+//     process.exit();
+// });
 
 // Log in to Discord with your client's token
 client.login(token);
