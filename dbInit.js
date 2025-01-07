f523734432f243c48ab5e68a4082f689a0326762
shirotohu <shirotohu@shirotohu.dev>
2025-01-07 12:46:31 +1000

    changed database to use one model

--- dbInit.js
+++ dbInit.js
@@ -7,11 +7,8 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 	storage: 'database/database.sqlite',
 });
 
-const Users = require('./models/Users.js')(sequelize, Sequelize.DataTypes);
-const UserScores = require('./models/UserScores.js')(sequelize, Sequelize.DataTypes);
+const Scores = require('./models/Scores.js')(sequelize, Sequelize.DataTypes);
 
 const force = process.argv.includes('--force') || process.argv.includes('-f');
 
-sequelize.sync({ force }).then(async () => {
-	sequelize.close();
-}).catch(console.error);
+sequelize.sync({ force });
