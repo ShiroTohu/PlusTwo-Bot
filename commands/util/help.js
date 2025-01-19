a2551d0f6d9e99e84982e31acc216f184a26ac57
shirotohu <shirotohu@shirotohu.dev>
2025-01-19 10:29:13 +1000

    added help command

---
+++ commands/util/help.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+
+const message = `
+### +2 Bot Functionality!
+Here are some ways you can alter someones score.
+ - Reply to someones message with \`+2\` or \`-2\`
+ - Use the \`/plustwo\` or \`/minustwo\` commands
+
+Remeber to have fun. By default users can only +2 someone 5 times a day, though can be disabled
+with \`/modoptions\`.
+`
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Get started shattering hopes and dreams'),
+	async execute(interaction) {
+		await interaction.reply({
+            content: message
+        });
+	},
+};
