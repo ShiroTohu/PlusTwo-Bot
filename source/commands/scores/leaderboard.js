04eaf617e24bfbc71b2c98d190fb702d721ed16b
shirotohu <shirotohu@shirotohu.dev>
2025-03-14 20:56:43 +1000

    replies working
    Users can now plusTwo/minusTwo with replies.

--- source/commands/scores/leaderboard.js
+++ source/commands/scores/leaderboard.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, bold } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { Guild } = require('../../database/database.js');
 const { logger } = require('../../logger.js');
 
--- source/events/messageCreate.js
+++ source/events/messageCreate.js
@@ -1,6 +1,7 @@
 const { Events } = require('discord.js');
-const { Guild } = require('../database/database');
+const { Guild, User, Score } = require('../database/database');
 const { logger } = require('../logger');
+
 // When a message is created it checks whether the type of the message is a reply.
 // It also checks whether the content is +2/-2 and alaters the score accordingly.
 module.exports = {
@@ -11,19 +12,26 @@ module.exports = {
         if (message.type == REPLY) {
             const reference = await message.fetchReference();
             const referenceAuthor = reference.author;
+            
+            const guild = await Guild.findOrCreate({where: {id: message.guildId}});
+		    await User.findOrCreate({where: {id: referenceAuthor.id, username: referenceAuthor.username}});
+		    await Score.findOrCreate({where: {UserId: referenceAuthor.id, GuildId: message.guildId}});
 
             if (referenceAuthor == message.author) {
+                await guild[0].minusTwo(referenceAuthor.id);
                 message.react('1325696373903065128');
-                return await alterScore(referenceAuthor, -2);
+                return;
             }
 
             switch (message.content) {
                 case "+2":
+                    await guild[0].plusTwo(referenceAuthor.id);
                     reference.react('1325696309671231561');
-                    return await alterScore(referenceAuthor, 2);
+                    return;
                 case "-2":
+                    await guild[0].minusTwo(referenceAuthor.id);
                     reference.react('1325696373903065128');
-                    return await alterScore(referenceAuthor, -2);
+                    return;
             }
         }
     }
