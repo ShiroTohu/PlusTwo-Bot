e1a707a5abb8cb4599018b3935f0a157a05cc7dd
shirotohu <shirotohu@shirotohu.dev>
2025-03-16 18:18:13 +1000

    tools folder added
    Allows you to add/delete the slash commands. Must be run in the root of
    the project otherwise it won't find the env file. That can be fixed in
    the future but for now I'm going to keep it that way.

--- tools/deleteCommands.js
+++ tools/deleteCommands.js
@@ -4,11 +4,6 @@ require('@dotenvx/dotenvx').config({path: ['../.env']});
 console.log(process.env.DISCORD_TOKEN);
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, 621532080228139019), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
-
 // for global commands
 rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
 	.then(() => console.log('Successfully deleted all application commands.'))
