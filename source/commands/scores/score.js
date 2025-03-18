b538034f023b4a7627ec8648bfee2dc2b3eaa8a9
shirotohu <shirotohu@shirotohu.dev>
2025-03-18 20:25:59 +1000

    added score command
    get's the user's score

--- source/commands/scores/score.js
+++ source/commands/scores/score.js
@@ -1,16 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { Users } = require('../../database/database');
+const { Score } = require('../../database/database');
+const { logger } = require('../../logger')
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('score')
 		.setDescription('Get how many plus two\'s you have'),
 	async execute(interaction) {
-		const score = await Users.findOne({
+		const score = await Score.findOne({
             attributes: ['score'],
-            where: {user_id: interaction.user.id}
+            where: {UserId: interaction.user.id, GuildId: interaction.guildId}
         });
 
-        interaction.reply(score);
+        interaction.reply(score.score.toString());
 	},
 };
