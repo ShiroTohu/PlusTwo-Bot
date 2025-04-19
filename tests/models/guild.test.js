3de9e02e2758597af3507bd9dda99f73f9aafebf
shirotohu <shirotohu@shirotohu.dev>
2025-04-19 10:19:55 +1000

    changed tab spacing to 4 in guild.test.js

--- tests/models/guild.test.js
+++ tests/models/guild.test.js
@@ -25,58 +25,58 @@ test('pass if guild model is able to be inserted into database', () => {
 });
 
 describe('Getter Setter methods', () => {
-  test('retrieving guild using getGuild method', async () => {
-    const guild = await Guild.getGuild(existingGuildId);
-    expect(guild).toBeTruthy();
-  });
+    test('retrieving guild using getGuild method', async () => {
+        const guild = await Guild.getGuild(existingGuildId);
+        expect(guild).toBeTruthy();
+    });
 
-  test('getting guild that does not exist', async () => {
-    await expect(Guild.getGuild('123549875487559172')).resolves.toBeFalsy();
-  });
+    test('getting guild that does not exist', async () => {
+        await expect(Guild.getGuild('123549875487559172')).resolves.toBeFalsy();
+    });
 
-  test('creating a guild using createGuild method', async () => {
-    const newGuildId = '912834509182370012';
-    await Guild.createGuild(newGuildId);
+    test('creating a guild using createGuild method', async () => {
+        const newGuildId = '912834509182370012';
+        await Guild.createGuild(newGuildId);
 
-    const guild = await Guild.getGuild(newGuildId);
+        const guild = await Guild.getGuild(newGuildId);
 
-    expect(guild).toBeTruthy();
-    expect(guild.id).toBe(newGuildId);
-    expect(Guild.findOne({where: {id: existingGuildId}})).resolves.toBeTruthy();
-  });
+        expect(guild).toBeTruthy();
+        expect(guild.id).toBe(newGuildId);
+        expect(Guild.findOne({where: {id: existingGuildId}})).resolves.toBeTruthy();
+    });
 });
 
 // TODO: Fix error with this test, for some reason it can't the guildId.
 describe('getLeaderboard method', () => {
-  test('returned objected from getLeaderboard method matches expected output', async () => {
-    const Guild = sequelize.models.Guild;
-    const guild = await Guild.getGuild(existingGuildId);
+    test('returned objected from getLeaderboard method matches expected output', async () => {
+        const Guild = sequelize.models.Guild;
+        const guild = await Guild.getGuild(existingGuildId);
 
-    expect(guild).toBeTruthy();
+        expect(guild).toBeTruthy();
 
-    const scores = await guild.getLeaderboard();
-    console.log(scores);
-    expect(scores).toBeTruthy();
-    expect(scores[0].User).toBeTruthy();
-  });
+        const scores = await guild.getLeaderboard();
+        console.log(scores);
+        expect(scores).toBeTruthy();
+        expect(scores[0].User).toBeTruthy();
+    });
 });
 
 describe('Guild score functionality', () => {
-  test('getScore method', async () => {
-    const guild = await Guild.getGuild(existingGuildId);
-    expect(guild.getScore(existingUserId)).resolves.toEqual(12);
-  })
+    test('getScore method', async () => {
+        const guild = await Guild.getGuild(existingGuildId);
+        expect(guild.getScore(existingUserId)).resolves.toEqual(12);
+    })
 
-  test('minus two', async () => {
-    const guild = await Guild.getGuild(existingGuildId);
-    await guild.minusTwo(existingUserId);
-    expect(guild.getScore(existingUserId)).resolves.toEqual(10);
-  });
+    test('minus two', async () => {
+        const guild = await Guild.getGuild(existingGuildId);
+        await guild.minusTwo(existingUserId);
+        expect(guild.getScore(existingUserId)).resolves.toEqual(10);
+    });
 
-  // This test relies on minus two. This isn't really ideal.
-  test('plus two', async () => {
-     const guild = await Guild.getGuild(existingGuildId);
-     await guild.plusTwo(existingUserId);
-     expect(guild.getScore(existingUserId)).resolves.toEqual(12);
-  });
+    // This test relies on minus two. This isn't really ideal.
+    test('plus two', async () => {
+        const guild = await Guild.getGuild(existingGuildId);
+        await guild.plusTwo(existingUserId);
+        expect(guild.getScore(existingUserId)).resolves.toEqual(12);
+    });
 });
