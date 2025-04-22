1800c8ee535b526350046712a012d37f6f50a781
shirotohu <shirotohu@shirotohu.dev>
2025-04-22 20:40:00 +1000

    updated parser
    Added --create-commands argument.

--- source/parser.js
+++ source/parser.js
@@ -8,8 +8,9 @@ program
     .name('Jerma Bot')
     .description('+2/-2 your friends with this funny little bot.')
     .version('0.0.0')
-    .option('-v, --verbose', 'increase the output of logs', 0)
+    .option('-v, --verbose', 'increase the output of logs.', 0)
     .option('--prod', 'Production database is used. requires .env to have login information.')
-    .option('--setup', 'env setup tool is triggered, useful for recondiguring the env file after initalization');
+    .option('--setup', 'env setup tool is triggered, useful for recondiguring the env file after initalization.')
+    .option('--create-commands', 'sends a request to discord to create commands, is REQUIRED to be ran before running the bot.')
 
 program.parse();
